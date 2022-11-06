b2074dc0cc24e15bfdc124ff4141b3bc971885d1
Ahmad Adelaja <ahmadadelaja@gmail.com>
2022-11-06 15:40:47 +0100

    Add simple for new user creation

--- conv-app-backend/accounts/tests.py
+++ conv-app-backend/accounts/tests.py
@@ -1,3 +1,34 @@
 from django.test import TestCase
+from .models import User
 
-# Create your tests here.
+
+class UserTests(TestCase):
+    """
+    Test the custom User model
+    """
+    def test_create_user(self):
+        user = User.objects.create_user(
+            username='wowu',
+            email='wowu@test.com',
+            password='test123'
+        )
+
+        self.assertEqual(user.username, 'wowu')
+        self.assertEqual(user.email, 'wowu@test.com')
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+
+    def test_create_superuser(self):
+        # Test if model can create super user successfully
+
+        admin_user = User.objects.create_superuser(
+            username='newadmin',
+            email='newadmin@test.com',
+            password='test123'
+        )
+        self.assertEqual(admin_user.username, 'newadmin')
+        self.assertEqual(admin_user.email, 'newadmin@test.com')
+        self.assertTrue(admin_user.is_active)
+        self.assertTrue(admin_user.is_staff)
+        self.assertTrue(admin_user.is_superuser)
